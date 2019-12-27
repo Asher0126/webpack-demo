72804a65c25486b457ec67bb62b792561ce89b9c
刘俊俊 <liujunjun@dongyin.net>
2019-12-27 15:35:37 +0800

    step11: 新增clean-webpack-plugin，删除构建目录垃圾文件

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,6 +1,13 @@
 const merge = require("webpack-merge");
 const webpackBaseConfig = require('./webpack.config.base');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(webpackBaseConfig, {
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+        // 清理 dist 目录
+        new CleanWebpackPlugin({
+            filename: './dist'
+        })
+    ]
 });
