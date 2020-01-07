cb64d4fdbf8270ed004b8f8bd33db6f9e68507a6
刘俊俊 <liujunjun@dongyin.net>
2020-01-07 11:30:46 +0800

    step19: 补充字体库font,svg等静态资源的使用

---
+++ src/shop/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h3>设置字体</h3>
+        <span class="iconfont iconadd"></span>
+
+        <svg class="icon" aria-hidden="true">
+            <use xlink:href="#iconfavorfill" />
+        </svg>
+    </div>
+</template>
+
+<script>
+import '../assets/fonts/iconfont.js';
+
+export default {
+    name: 'Index'
+};
+</script>
+
+<style lang="scss">
+@import '../assets/fonts/iconfont.css';
+</style>
